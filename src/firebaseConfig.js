365ae70f7faa8b97aff858ead539eaa3d9c9a7bb
Sagana Jeyakumar <saganajeyakumar@Saganas-MacBook-Pro.local>
2024-09-16 11:57:00 -0700

    Move to new firebase table

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCiUfaDudLOgZ1PFKVuXF4lOywPlOz9pkE",
-    authDomain: "birthdayinvite-f00d0.firebaseapp.com",
-    projectId: "birthdayinvite-f00d0",
-    storageBucket: "birthdayinvite-f00d0.appspot.com",
-    messagingSenderId: "414750589442",
-    appId: "1:414750589442:web:45340d0dcb3b7097941d1b"
+  apiKey: "AIzaSyAFAUZWqTLuXO-Urx7RpYQ3mEaTe5XEJ8Y",
+  authDomain: "mermaid-invite.firebaseapp.com",
+  projectId: "mermaid-invite",
+  storageBucket: "mermaid-invite.appspot.com",
+  messagingSenderId: "99004880447",
+  appId: "1:99004880447:web:8df3571444858e955c4a8a"
   };
 
   const app = initializeApp(firebaseConfig);
