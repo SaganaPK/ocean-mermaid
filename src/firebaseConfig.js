fa2288d5d181b1766b9130e4afe4d77f124f8497
Sagana Jeyakumar <saganajeyakumar@Saganas-MacBook-Pro.local>
2024-09-19 16:24:10 -0700

    final db update

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,13 +4,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFAUZWqTLuXO-Urx7RpYQ3mEaTe5XEJ8Y",
-  authDomain: "mermaid-invite.firebaseapp.com",
-  projectId: "mermaid-invite",
-  storageBucket: "mermaid-invite.appspot.com",
-  messagingSenderId: "99004880447",
-  appId: "1:99004880447:web:8df3571444858e955c4a8a"
-  };
+  apiKey: "AIzaSyCgzyArPrDkYdTAsKcmkkdDpYc3eXd0fwY",
+  authDomain: "nilanithilabirthday.firebaseapp.com",
+  projectId: "nilanithilabirthday",
+  storageBucket: "nilanithilabirthday.appspot.com",
+  messagingSenderId: "730666870740",
+  appId: "1:730666870740:web:f12d28883814ef23115841"
+};
 
   const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
